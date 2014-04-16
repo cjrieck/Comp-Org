39a03670dd46648a3998281d5302e650e8b0519a
Clayton Rieck <cjrieck123@gmail.com>
2014-04-16 16:32:02 -0400

    starting SPIM 3

---
+++ Assignment 3/fib.py
@@ -0,0 +1,25 @@
+def fast_fib(n, p1, p2, summation):
+
+	print p2
+	summation += p2 # increment summation 
+
+	if n == 1: # if down to the last term
+		return p2, summation # return nth fib number and sum
+	else:
+		return fast_fib(n-1, p2, p1+p2, summation) # pass fib term-1, fi-2, fi-1, current sum
+
+
+def fib(n, summation):
+
+	if n == 0:
+		return 0, summation
+
+	else:
+		return fast_fib(n, 0, 1, 0)
+
+def main():
+
+	print fib(5, 0)
+
+if __name__ == '__main__':
+	main()
